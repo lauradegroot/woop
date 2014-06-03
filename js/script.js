a660b3a60bb06405c8f40081b8bae6cfb89cb1bb
Laura DeGroot <laurardegroot@gmail.com>
2014-06-03 14:57:54 -0400

    commenting js

--- js/script.js
+++ js/script.js
@@ -2,31 +2,24 @@ $(function(){
 	useQuerySrc();	
 });
 
-function useQuerySrc(){
-	var imageSrc = getUrlVars()["src"];
-	if (undefined !== imageSrc && imageSrc.length){
-		$('.image img:first-child').replaceWith("<img src='"+imageSrc+"'/>");
-		woop();
-	} else{
-		woop();
-	}
-}
+// first image before being copied, doesn't get border radius or animate. width and height are 100%.
+var firstImage = $('.image img:first-child');
 
 function woop(){	
-	var levels = getUrlVars()["levels"];
+	var levels = getUrlVars()["levels"]; // are number of levels specified?
 	for (var i=0; i < (levels || 15 ) ; i++){
-		var image = $('.image img:last-child');
-		var newImage = image.clone().appendTo('.image');
+		var image = $('.image img:last-child'); 
+		var newImage = image.clone().appendTo('.image'); // duplicate last image and append to images
 		var prevImage = newImage.prev();
-		var prevWidth = prevImage.width();
-		var newWidth = (prevWidth / 120) * 100;
-		newImage.css('width', newWidth);
+		var prevWidth = prevImage.width(); // get width of image above new image
+		var newWidth = (prevWidth / 120) * 100; 
+		newImage.css('width', newWidth); // set width of new image
 	}
 	setShape();
 	setAnimate();
 }
 
-function getUrlVars(){
+function getUrlVars(){ // get query string parameters
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     for(var i = 0; i < hashes.length; i++)
@@ -38,22 +31,32 @@ function getUrlVars(){
     return vars;    
 }
 
+function useQuerySrc(){
+	var imageSrc = getUrlVars()["src"]; // is an alternate image src specified?
+	if (undefined !== imageSrc && imageSrc.length){
+		firstImage.replaceWith("<img src='"+imageSrc+"'/>");
+		woop();
+	} else{
+		woop();
+	}
+}
+
 function setShape(){
-	var shape = getUrlVars()["shape"];
+	var shape = getUrlVars()["shape"]; // is a shape specified?
 	if ( shape === "square" ) {
-		$('.image img:first-child').siblings().css('border-radius','none');
+		firstImage.siblings().css('border-radius','none');
 	} else {
-		$('.image img:first-child').siblings().css('border-radius','100%');
+		firstImage.siblings().css('border-radius','100%');
 	}
 }
 
 function setAnimate(){
-	var animate = getUrlVars()["animate"];
+	var animate = getUrlVars()["animate"]; // are we animating this baby?
 	if ( animate === "yes" ) {
-		$('.image img:nth-child(odd)').addClass('animate-odd');
-		$('.image img:nth-child(even)').addClass('animate-even');
-		$('.image img:first-child').removeClass();
+		$('.image img:nth-child(odd)').addClass('animate-odd'); // odd numbered images go clockwise
+		$('.image img:nth-child(even)').addClass('animate-even'); // even numbered images go counter clockwise
+		firstImage.removeClass(); // first image stays put
 	} else {
-		$('.image img:nth-child(even)').siblings().removeClass();
+		$('.image img').removeClass();
 	}
 }
