6934ea4de70cc3e1839b3c9a24c7fd4b5cf5d663
Laura DeGroot <laurardegroot@gmail.com>
2014-06-10 12:07:56 -0400

    getUserMedia for media source working with all features

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@ $(function(){
 	useQuerySrc();	
 });
 
-// first image before being copied, doesn't get border radius or animate. width and height are 100%.
-var firstImage = $('.image img:first-child');
+var source = $('.source');
 
 function woop(){	
 	var levels = getUrlVars()["levels"]; // are number of levels specified?
 	for (var i=0; i < (levels || 15 ) ; i++){
-		var image = $('.image img:last-child'); 
-		var newImage = image.clone().appendTo('.image'); // duplicate last image and append to images
-		var prevImage = newImage.prev();
-		var prevWidth = prevImage.width(); // get width of image above new image
+		var lastSource = $('.sources .source:last-child'); 
+		var newSource = lastSource.clone().appendTo('.sources'); // duplicate last media source and append to images
+		var prevSource = newSource.prev();
+		var prevWidth = prevSource.width(); // get width of media source above new media source
 		var newWidth = (prevWidth / 120) * 100; 
-		newImage.css('width', newWidth); // set width of new image
+		newSource.css('width', newWidth); // set width of new media source
 	}
 	setShape();
 	setAnimate();
@@ -22,8 +21,7 @@ function woop(){
 function getUrlVars(){ // get query string parameters
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for(var i = 0; i < hashes.length; i++){
         hash = hashes[i].split('=');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
@@ -32,11 +30,16 @@ function getUrlVars(){ // get query string parameters
 }
 
 function useQuerySrc(){
+	var videoSrc = getUrlVars()["video"];
 	var imageSrc = getUrlVars()["src"]; // is an alternate image src specified?
 	if (undefined !== imageSrc && imageSrc.length){
-		firstImage.replaceWith("<img src='"+imageSrc+"'/>");
+		$('.source:first-child').replaceWith("<img src='"+imageSrc+"' class='source'/>");
 		woop();
-	} else{
+	} else if ( videoSrc === "yes") {
+		$('.source:first-child').replaceWith("<video autoplay class='source'></video>");
+		getVideo();
+	}
+	else{
 		woop();
 	}
 }
@@ -44,19 +47,42 @@ function useQuerySrc(){
 function setShape(){
 	var shape = getUrlVars()["shape"]; // is a shape specified?
 	if ( shape === "square" ) {
-		firstImage.siblings().css('border-radius','none');
+		$('.source:first-child').siblings().css('border-radius','none');
 	} else {
-		firstImage.siblings().css('border-radius','100%');
+		$('.source:first-child').siblings().css('border-radius','100%');
 	}
 }
 
 function setAnimate(){
 	var animate = getUrlVars()["animate"]; // are we animating this baby?
 	if ( animate === "yes" ) {
-		$('.image img:nth-child(odd)').addClass('animate-odd'); // odd numbered images go clockwise
-		$('.image img:nth-child(even)').addClass('animate-even'); // even numbered images go counter clockwise
-		firstImage.removeClass(); // first image stays put
+		$('.sources .source:nth-child(odd)').addClass('animate-odd'); // odd numbered media sources go clockwise
+		$('.sources .source:nth-child(even)').addClass('animate-even'); // even numbered media sources go counter clockwise
+		$('.source:first-child').removeClass(); // first media source stays put
 	} else {
-		$('.image img').removeClass();
+		$(source).removeClass('animate-odd');
+		$(source).removeClass('animate-even');
+	}
+}
+
+function getVideo(){
+ 	var errorCallback = function(e) {  
+	   alert('awww, do it', e); // alert when user does not allow cam
+	 };
+
+	navigator.getUserMedia  = navigator.getUserMedia ||
+	                          navigator.webkitGetUserMedia ||
+	                          navigator.mozGetUserMedia ||
+	                          navigator.msGetUserMedia;
+
+	var video = document.querySelector('video');
+
+	if (navigator.getUserMedia) {
+	  navigator.getUserMedia({audio: true, video: true}, function(stream) {
+	    video.src = window.URL.createObjectURL(stream);
+	    woop();
+	  }, errorCallback);
+	} else {
+	  alert("you can't webrtc with your browser") // alert when user's browser doesnt support webrtc
 	}
 }
